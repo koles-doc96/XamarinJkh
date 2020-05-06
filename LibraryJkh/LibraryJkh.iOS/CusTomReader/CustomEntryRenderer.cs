6be72cb708799b9a1023e6d6f65d18d5af808ef4
evgen <koles.doc96.09.27@gmail.com>
2020-05-06 18:38:37 +0700

    Инициализация проекта + верска формы Авторизации

---
+++ LibraryJkh/LibraryJkh.iOS/CusTomReader/CustomEntryRenderer.cs
@@ -0,0 +1,17 @@
+﻿using LibraryJkh.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace LibraryJkh.iOS
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
---
+++ LibraryJkh/LibraryJkh/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace LibraryJkh
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            BtnLogin.Text = "Войдено";
+        }
+    }
+}
